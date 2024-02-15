51931f9410b911d2ab59f20ced77e1f69ca212c4
GsnMithra <mithragsn9@gmail.com>
2024-02-15 22:48:51 +0530

    n_ctx update

--- llamaMethods.py
+++ llamaMethods.py
@@ -8,7 +8,7 @@ import pytesseract
 from PIL import Image
 
 MODEL_PATH = "./model/llama-2-7b-chat.Q8_0.gguf"
-
+n_ctx = 2048
 
 def getModel ():
     callbacks = CallbackManager ([StreamingStdOutCallbackHandler ()])
@@ -18,59 +18,67 @@ def getModel ():
         n_gpu_layers=1000,
         max_tokens=1000,
         n_batch=4096,
-        n_ctx=2048,
+        n_ctx=n_ctx,
         top_p=1,
         callback_manager=callbacks,
-        verbose=True,
+        verbose=True
     )
-    
+
     return model
-    
-def getTextFromPDF (file):
+
+def getTextFromPDF (files):
     text = ""
-    if str (file.name).split (".")[-1] == "pdf":
-        pdf_reader = PyPDF2.PdfReader (file)
-        for page_num in range (len (pdf_reader.pages)):
-            text += pdf_reader.pages[page_num].extract_text ()
-        orgtext = " ".join (text.split ())
-        text = orgtext
-    elif (
-        str (pdf_file.name).split (".")[-1] == "jpg"
-        or str (pdf_file.name).split (".")[-1] == "jpeg"
-        or str (pdf_file.name).split (".")[-1] == "png"
-    ):
-        photo = Image.open (file)
-        docs = pytesseract.image_to_string (photo)
-        text = docs
-        
+
+    for file in files:
+        if str (file.name).split (".")[-1] == "pdf":
+            pdf_reader = PyPDF2.PdfReader (file)
+            for page_num in range (len (pdf_reader.pages)):
+                text += pdf_reader.pages[page_num].extract_text ()
+            orgtext = " ".join (text.split ())
+            text = orgtext
+        elif (
+            str (pdf_file.name).split (".")[-1] == "jpg"
+            or str (pdf_file.name).split (".")[-1] == "jpeg"
+            or str (pdf_file.name).split (".")[-1] == "png"
+        ):
+            photo = Image.open (file)
+            docs = pytesseract.image_to_string (photo)
+            text = docs
+
     return text
-    
-def loadModel (file, params):
+
+def loadModel (files, params):
     llm = getModel ()
     minLength = params["min"]
     maxLength = params["max"]
-    
+
     template = """
         [INST] <<SYS>>
         You are tasked with summarizing the following text in a detailed manner within {min} to {max} words. Please ensure that your responses are socially unbiased and positive in nature.
         <</SYS>>
         {text}[/INST]
         """.strip()
-        
+
     prompt = PromptTemplate (
         template=template,
         input_variables=["text", "min", "max"]
     )
-        
+
     llm_chain = LLMChain (
         prompt=prompt,
         llm=llm
     )
     
-    model_response = llm_chain.run (
-        text=getTextFromPDF (file),
-        min=minLength,
-        max=maxLength
-    )
-    
-    return model_response
+    context_text = getTextFromPDF (files)
+    model_responses = list ()
+    if len (context_text) >= n_ctx:
+        for i in range (0, len (context_text), n_ctx):
+            model_responses.append (
+                llm_chain.run (
+                    text=context_text[i:i+n_ctx],
+                    min=minLength - 30,
+                    max=maxLength - 30
+                )
+            )
+
+    return ''.join (model_responses)
--- main.py
+++ main.py
@@ -3,18 +3,11 @@ from llamaMethods import loadModel
 
 if __name__ == "__main__":
     stm.title("PDF Summarizer")
-    
+
     with stm.sidebar:
         stm.header("Options")
-        file = stm.file_uploader("Upload a PDF file", type=["pdf"], accept_multiple_files=True)
-        
-        # three options describing the summarized length of the output
-        # short, medium, long
-        # short: 20-50 words
-        # medium: 50-100 words
-        # long: 100-200 words
-        # default: medium
-    
+        files = stm.file_uploader("Upload a PDF file", type=["pdf"], accept_multiple_files=True)
+
         stm.header("Summarized Length")
         length = stm.radio("Select the length of the summary", ["Short", "Medium", "Long"], index=1)
         if length == "Short":
@@ -23,9 +16,8 @@ if __name__ == "__main__":
             params = { "min": 50, "max": 100 }
         elif length == "Long":
             params = { "min": 100, "max": 200 }
-        
-    if file is not None:
+
+    if files is not None:
         if stm.button("Summarize"):
-            for chunk in file:
-                summary = loadModel (chunk, params)
-                stm.write(summary)
+            response = loadModel(files, params)
+            stm.write(response)
